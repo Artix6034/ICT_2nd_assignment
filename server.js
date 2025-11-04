ee1abd8b399e750a0587471d099fad73ad492882
artix6034 <166250865+Artix6034@users.noreply.github.com>
2025-11-04 20:04:58 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { connectDB } from './config/db.js'; // ✅ correct path
 require('dotenv').config();
 
 const { connectDB } = require('./config/db'); // make sure path is config/db.js
